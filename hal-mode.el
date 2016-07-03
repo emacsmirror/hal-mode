eeeba86efc7a41a013b8749a45b9db3b21d3d7d2
Alexander Rössler <mail.aroessler@gmail.com>
2016-07-03 19:48:14 +0200

    using Emacs formatting standard

--- hal-mode.el
+++ hal-mode.el
@@ -1,5 +1,35 @@
-;; emacs mode for Machinekit HAL files
-;; by Alexander Rössler <mail AT roessler DOT systems>
+;;; hal-mode.el --- Major mode for editing HAL files -*- lexical-binding: t; -*-
+
+;; Author: Alexander Rössler
+;; URL: https://github.com/strahlex/hal-mode/
+;; Version: 0.1
+;; Package-Requires: ((generic-x "19.0"))
+;; Keywords: language
+
+;; Copyright (C) 2016 Alexander Rössler
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; To install, see https://github.com/strahlex/hal-mode/blob/master/README.md
+;;
+;; Known bugs:
+;;
+;; * None
+
+;;; Code:
 
 (require 'generic-x)
 
